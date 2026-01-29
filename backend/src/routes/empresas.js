35ec474e9f0de61de901346b7a9971067750fe92
LuisRamírez <3498173080101@ingenieria.usac.edu.gt>
2026-01-29 17:17:36 -0600

    ruta de paises y filiales

---
+++ backend/src/routes/empresas.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const supabase = require('../config/supabase');
+
+
+
+module.exports = router;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/paises', require('./paises'));
+router.use('/empresas', require('./empresas'));
 router.use('/filiales', require('./filiales'));
 router.use('/sedes', require('./sedes'));
 router.use('/encuestas'), require('./encuestas');
