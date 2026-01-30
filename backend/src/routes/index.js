4906b304fbfdba447b219824b40d70ed7dc8695c
LuisRamírez <3498173080101@ingenieria.usac.edu.gt>
2026-01-29 18:32:38 -0600

    logica funcional completada

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.use('/paises', require('./paises'));
 router.use('/empresas', require('./empresas'));
-router.use('/filiales', require('./filiales'));
 router.use('/sedes', require('./sedes'));
 router.use('/preguntas', require('./preguntas'));
 router.use('/encuestas', require('./encuestas'));
